0de0b3dbf539a8638f9c458f12e6247dcb73cac4
POSTPADI <106396204+FahmiIrsyad@users.noreply.github.com>
2024-06-18 08:53:31 +0800

    Update Movie.php

--- Movie.php
+++ Movie.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'release',
+        'length',
+        'description',
+        'mpaa_rating',
+        'genre',
+        'director',
+        'performer',
+        'language',
+    ];
 }
+
